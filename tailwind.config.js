f03ff1cebf85e91ed8fd1e4fb94b9bdfd92a3eda
ChaoAlarcón <chaoalarcon03@gmail.com>
2025-04-20 17:35:48 +0200

    Creando Pseudoelementos

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,12 @@ export default {
 
             backgroundImage: {
                 circles: "url('/src/assets/circles.svg')",
-            }
+            },
+
+            height: {
+                header: "400px",
+
+            },
         },
     },
     plugins: [],
