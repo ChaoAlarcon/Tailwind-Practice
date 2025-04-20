24df9def466b45379498fa8da81d2e228d5b6bad
ChaoAlarcón <chaoalarcon03@gmail.com>
2025-04-20 17:52:20 +0200

    Empiezo de 0 con una versión anterior de Tailwind

--- README.md
+++
@@ -1,8 +0,0 @@
-# Tailwind-Practice
-
-## Este es un proyecto creado siguiendo el curso de [Adrián Maza Vázquez](https://www.linkedin.com/in/adrian-maza-vazquez/)
-
----
-### Notas:
-
--Aprender a usar el [tailwind.config.js](tailwind.config.js) para crear nuevos colores y elementos.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        "./src/**/*.html", // Adjust the paths to match your project structure
-    ],
-    theme: {
-        extend: {
-            colors:{
-                green: "#1DF299"
-            },
-
-            backgroundImage: {
-                circles: "url('/src/assets/circles.svg')",
-            },
-
-            height: {
-                header: "400px",
-
-            },
-        },
-    },
-    plugins: [],
-};
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
