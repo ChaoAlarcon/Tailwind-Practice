4fb5da49b27d0774a5770eb0d9f93d217ecaeb6f
ChaoAlarcón <chaoalarcon03@gmail.com>
2025-04-20 20:14:33 +0200

    Diseño Responsive en Tailwind 3.4.17

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
  export default {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+
+      screens:{
+
+        tv: "1400px",
+        
+      },
+
+      height:{
+        header: "200px",
+        "header-phone": "1000px",
+      },
+      colors: {
+        primary: "#FF6B00",
+        
+      },
+      
+    },
   },
   plugins: [],
 }
